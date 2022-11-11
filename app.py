10bc28f6eaccfaf3000bfcc8195c827eb6453d57
jamesvillecadf <107677335+jamesvillecadf@users.noreply.github.com>
2022-11-10 23:40:41 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
-app = Flask(__name__)
+from django.http import JsonResponse
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Koyeb'
+from django.contrib.auth import authenticate, login, logout
+
+def signin( request):
+    userName = request.POST.get('username')
+    passWord = request.POST.get('password') 
+
+    if user is not None:
+        if user.is_active:
+            if user.is_superuser:
+                login(request, user)
+                request.session['usertype'] = 'mgr'
+
+                return JsonResponse({'ret': 0})
+            else:
+                return JsonResponse({'ret': 1, 'msg': 'Please log in with an administrator account'})
+        else:
+            return JsonResponse({'ret': 0, 'msg': 'User has been disabled'})
+        
+    else:
+        return JsonResponse({'ret': 1, 'msg': 'Username or password is incorrect'})
 
 
-if __name__ == "__main__":
-    app.run()
+def signout( request):
+    logout(request)
+    return JsonResponse({'ret': 0})
